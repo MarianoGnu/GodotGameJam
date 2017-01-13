84fcea243cd522de5094974f4787204344ab81d3
Bruno SXS <brunosxs@gmail.com>
2017-01-13 19:27:45 -0200

    fixed bug in damage by a bomb

--- scenes/items/bomb/bomb_instance.gd
+++ scenes/items/bomb/bomb_instance.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 func _on_area_body_enter( body ):
 	if body.is_in_group("enemy") or body.is_in_group("player"):
-		body.take_damage(1)
+		body.take_damage(1,self)
 	if body.is_in_group("cracked_wall"):
 		# TODO: break the wall
 		pass
