c0a7c2ce16f7243b5a4a94f0802a184ed1ddea55
Bruno SXS <brunosxs@gmail.com>
2017-01-13 14:24:06 -0200

    fixed disappearing HUD after intro creation

--- scenes/HUD/HUD.gd
+++ scenes/HUD/HUD.gd
@@ -1,6 +1,6 @@
 tool
 
-extends Control
+extends CanvasLayer
 
 onready var btnA = get_node("btnA")
 onready var btnB = get_node("btnB")
@@ -8,10 +8,18 @@ onready var textureA = get_node("btnA/textureA")
 onready var textureB = get_node("btnB/textureB")
 
 func _ready():
-	
+	print(get_property_list())
 	pass
 
 func _on_btnA_draw():
 	btnA.draw_circle(btnA.get_size()/2, btnA.get_size().x/2, Color(0,1,0,1) )
 func _on_btnB_draw():
 	btnB.draw_circle(btnB.get_size()/2, btnB.get_size().x/2, Color(1,0,0,1) )
+
+func show():
+	set_layer(1)
+	pass
+func hide():
+	set_layer(-2)
+	pass
+		
